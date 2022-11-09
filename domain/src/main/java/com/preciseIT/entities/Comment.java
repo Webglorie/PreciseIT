4b492b287ad7e234cc0d744c886c813ae272265b
Rijnders,Bart B <b.rijnders@student.fontys.nl>
2022-11-09 17:07:20 +0100

    Implementend the Comment class

--- domain/src/main/java/com/preciseIT/entities/Comment.java
+++ domain/src/main/java/com/preciseIT/entities/Comment.java
@@ -1,16 +1,28 @@
 package com.preciseIT.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.jpa.domain.AbstractAuditable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import java.util.UUID;
+import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "comment")
-public class Comment extends AbstractAuditable<User, UUID> {
+public class Comment extends AbstractAuditable<User, Integer> {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue
+    private Integer id;
 
     @Column(name = "text")
     private String text;
+
+
 }
