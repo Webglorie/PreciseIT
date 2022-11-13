eae86c7a6f755e7c98bc0d823a209257b4110b4e
Webglorie <98421320+Webglorie@users.noreply.github.com>
2022-11-13 23:17:40 +0100

    Changed the Dummydata to be able to test the authorizations

--- H2persistence/src/main/java/com/preciseIT/DummyData.java
+++ H2persistence/src/main/java/com/preciseIT/DummyData.java
@@ -2,6 +2,7 @@ package com.preciseIT;
 
 import com.preciseIT.entities.User;
 import com.preciseIT.entities.Ticket;
+import com.preciseIT.enums.Role;
 import com.preciseIT.repos.TicketRepository;
 import com.preciseIT.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Component;
 public class DummyData {
 
     public DummyData(@Autowired UserRepository UserRepository, @Autowired TicketRepository ticketRepository) {
-        User appUser1 = new User("b.rijnders@thedoc.nl", "Bart", "Rijnders", "password");
+        User appUser1 = new User("b.rijnders@thedoc.nl", "Bart", "Rijnders", "password", Role.ADMIN);
         UserRepository.save(appUser1);
-        User appUser2 = new User("Testarjen@gmail.com", "Arjen", "Froma", "password", "YJXWKFYWYO");
+        User appUser2 = new User("Testarjen@gmail.com", "Arjen", "Froma", "password", "YJXWKFYWYO", Role.CLIENT);
         UserRepository.save(appUser2);
-        User appUser3 = new User("282859@thedoc.nl", "Bart", "Rijnders", "password");
+        User appUser3 = new User("282859@thedoc.nl", "Bart", "Rijnders", "password", Role.COMPANYADMIN);
         UserRepository.save(appUser3);
+        User appUser4 = new User("Testadmin@gmail.com", "Arjen", "Froma", "password", "TTEO2UXZ7X", Role.ADMIN);
+        UserRepository.save(appUser4);
         Ticket ticket1 = new Ticket("hulp nodig bij starten laptop", appUser2, appUser1);
         ticketRepository.save(ticket1);
         Ticket ticket2 = new Ticket("Word crasht na invoeren hoofdletter", appUser3, appUser1);
