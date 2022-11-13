54013ad24f336994704025993d8aa1a3b32d5b2c
Webglorie <98421320+Webglorie@users.noreply.github.com>
2022-11-13 23:21:56 +0100

    Small ticket changes for testing

--- api-service/src/main/java/com/preciseIT/controllers/TicketController.java
+++ api-service/src/main/java/com/preciseIT/controllers/TicketController.java
@@ -1,37 +1,46 @@
 package com.preciseIT.controllers;
 
-import com.preciseIT.entities.Ticket;
+import com.preciseIT.auth.service.TicketService;
+import com.preciseIT.auth.service.UserService;
+import com.preciseIT.entities.Auditable;
 import com.preciseIT.entities.User;
-import com.preciseIT.repos.TicketRepository;
-import com.preciseIT.repos.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.preciseIT.entities.Ticket;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/tickets")
+@Controller
+@RequestMapping("/portal/tickets")
 public class TicketController {
-    //todo implementation
-    private final TicketRepository ticketRepository;
-    private final UserRepository userRepository;
 
-    public TicketController(@Autowired TicketRepository ticketRepository, UserRepository userRepository) {
-        this.ticketRepository = ticketRepository;
-        this.userRepository = userRepository;
-    }
+    @Resource
+    private TicketService ticketService;
+    @Resource
+    private UserService userService;
 
     @GetMapping
     public Iterable<Ticket> getAllTickets() {
-         Iterable<Ticket> tickets = ticketRepository.findAll();
+         Iterable<Ticket> tickets = ticketService.findAllTickets();
          return tickets;
+    }
+
+    @GetMapping("/all")
+    public String listAllTickets(Model model){
+        List<Ticket> tickets = (List<Ticket>) ticketService.findAllTickets();
+        model.addAttribute("tickets", tickets);
+        return "portal/portal-tickets";
     }
 
     @GetMapping("/{id}")
     public Ticket getTicketById(@PathVariable Integer id){
-        Optional<Ticket> optionalTicket = ticketRepository.findById(id);
+        Optional<Ticket> optionalTicket = ticketService.findTicketById(id);
         if (optionalTicket.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No ticket found with this id.");
         } else {
@@ -41,19 +50,16 @@ public class TicketController {
 
     @PostMapping()
     public Ticket createNewTicket(@RequestBody Ticket ticket){
-        return ticketRepository.save(ticket);
+        return ticketService.saveTicket(ticket);
     }
 
     @PostMapping("/create-ticket/{title}/{createdById}")
-    public boolean register(@PathVariable String title, @PathVariable String createdById) {
-        User user = new User("Testuser", "test", "test");
-        userRepository.save(user);
-        Ticket ticket = new Ticket();
-        ticket.setTitle(title);
-        ticket.setAssignee(user);
-        ticketRepository.save(ticket);
-
-        return true;
+    public String register(HttpServletRequest request, @PathVariable String title, @PathVariable String createdById) {
+        User user1 = userService.findByEmail(request.getUserPrincipal().getName());
+        userService.saveUser(user1);
+        Ticket ticket = new Ticket(title, new Auditable<>());
+        ticketService.saveTicket(ticket);
+        return "portal/portal-tickets";
     }
 
 }
--- domain/src/main/java/com/preciseIT/entities/Ticket.java
+++ domain/src/main/java/com/preciseIT/entities/Ticket.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.data.jpa.domain.AbstractAuditable;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -17,9 +18,19 @@ import java.util.List;
 @Table(name = "ticket")
 public class Ticket extends AbstractAuditable<User, Integer> {
     public Ticket(String title, User questioner, User assignee) {
+        this.setCreatedDate(LocalDateTime.now());
+        this.setLastModifiedDate(LocalDateTime.now());
+        this.setCreatedBy(questioner);
         this.title = title;
         this.questioner = questioner;
         this.assignee = assignee;
+    }
+
+    public Ticket(String title, Auditable<User> userAuditable) {
+        this.title = title;
+        this.setCreatedBy(userAuditable.getCreatedBy());
+        this.setCreatedDate(LocalDateTime.now());
+        this.setLastModifiedDate(LocalDateTime.now());
     }
 
     @Column(name = "title", nullable = false)
