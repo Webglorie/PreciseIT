b2a27fce269363fb2230c03e22450efac9371406
Webglorie <98421320+Webglorie@users.noreply.github.com>
2022-11-13 23:14:28 +0100

    Added logged in email in header

--- pit-website/src/main/java/com/preciseIT/webapp/controller/FragmentsController.java
+++ pit-website/src/main/java/com/preciseIT/webapp/controller/FragmentsController.java
@@ -1,11 +1,19 @@
 package com.preciseIT.webapp.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+
+@Controller
 public class FragmentsController {
     @GetMapping("/fragments")
-    public String getHome() {
-        return "fragments.html";
+    public String getHome(HttpServletRequest request, Model model) {
+        model.addAttribute("username", request.getUserPrincipal().getName());
+        return "fragments/general";
     }
 }
